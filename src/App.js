1ef21ed973fc82fd9fd34e9a4c3132892c2b296d
Ankit Mahule <ank.mahule@gmail.com>
2024-12-08 01:42:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [responseObj, setResponseObj] = useState([]);
+  const [file, setFile] = useState([]);
+  async function getFileData() {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("fileName", file.name);
+    console.log(formData);
+    const response = await fetch("http://localhost:8080/read-resume", {
+      method: "POST",
+      body: formData,
+    });
+    const test = await response.json();
+    setResponseObj(test?.fileData);
+  }
+
+  function onFileChange(event) {
+    console.log(event.target.files[0]);
+    setFile(event.target.files[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <input type="file" name="resume-file" onChange={onFileChange}></input>
+
+        <button type="button" onClick={() => getFileData()}>
+          Upload
+        </button>
+      </div>
+      {!responseObj ? null : <div className="file-content">{responseObj}</div>}
+    </>
   );
 }
 
